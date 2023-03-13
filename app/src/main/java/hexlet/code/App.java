a630df869e2c15a6c339611a948072f677be7dec
Pavel Muravyev <muravyev.pavel@gmail.com>
2023-03-13 21:11:51 +0300

    Added basic Javalin configuration.

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,35 @@
 package hexlet.code;
 
+import hexlet.code.controllers.RootController;
+import io.javalin.Javalin;
+
 public class App {
 
     public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
 
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "8080");
+        return Integer.parseInt(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", RootController.helloWorld);
     }
 }
