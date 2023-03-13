58f5af849b960de0b4abdbfc3674ba8547b0edbe
Pavel Muravyev <muravyev.pavel@gmail.com>
2023-03-13 14:05:35 +0300

    Initial commit.

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class AppTest {
+
+    @Test
+    void main() {
+        assertTrue(true);
+        assertFalse(false);
+    }
+}
